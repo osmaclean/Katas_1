62c11753d1ee2db008f544d87373f0c61aeb619e
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-08 10:38:21 -0300

    Projeto iniciado (Atualizando)

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// Primeira Tarefa
+
+function cubicSquare() {
+    let numbers = [];
+    for (let i = 0; i <= 10; i++) {
+            numbers.push(Math.pow(i,3));
+         }
+    return numbers
+}
+console.log(cubicSquare());
+
+/////////////////////////////////
